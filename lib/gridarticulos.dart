d2f284385499175defd80f58b9c2dc76b6dc9c5d
FlutLab.io <>
2022-05-02 18:23:35 +0000

    Autosave

--- lib/gridarticulos.dart
+++ lib/gridarticulos.dart
@@ -37,7 +37,7 @@ class _PaginaInicialState extends State<PaginaInicial> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Flutter GridView"),
+          title: const Text("Listado de Articulos"),
         ),
         body: GridView.builder(
           itemCount: images.length,
